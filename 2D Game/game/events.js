1844047e0e2372e65d6601f704cbbb8414f3488b
hist-site <63604237+hist-site@users.noreply.github.com>
2021-11-24 05:34:13 +0200

    in com

--- 2D Game/game/events.js
+++ 2D Game/game/events.js
@@ -1,7 +1,7 @@
 import { config } from "./config.js";
 
 window.addEventListener('keypress', (e) => {
-    if (e.key == 'g') {
+    if (e.code == 'KeyG') {
         console.log('sd');
         config.gridState = !config.gridState
     }
