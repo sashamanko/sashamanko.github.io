49671498930f5317cfa9c57161d08f5c5d2549cf
hist-site <63604237+hist-site@users.noreply.github.com>
2021-12-01 15:27:14 +0200

    in com

--- todo-app/assets/js/app.js
+++ todo-app/assets/js/app.js
@@ -1,10 +1,13 @@
 const input = document.querySelector('.input-block__input'),
     input_btn = document.querySelector('.input-block__btn'),
+    propsList = document.querySelector('.props-list'),
     propsItem = document.querySelectorAll('.props-list__item'),
     propsMenu = document.querySelector('.props-menu');
 
+const propsArray = []
+
 input.addEventListener('input', () => {
-    if (input.value.length !== 0) {
+    if (input.value.length !== 0 && filter(input.value)) {
         input_btn.classList.add('d-flex');
     } else {
         input_btn.classList.remove('d-flex');
@@ -14,5 +17,43 @@ input.addEventListener('input', () => {
 propsItem.forEach(elem => {
     elem.addEventListener('click', (e) => {
         propsMenu.classList.add('active');
+
     })
 })
+
+input_btn.addEventListener('click', () => {
+    const props = new Props(input.value);
+    propsArray.push(props);
+    props.pushProps();
+    input.value = '';
+    input_btn.classList.remove('d-flex');
+
+})
+
+class Props {
+    constructor(title) {
+        this.title = title;
+    }
+
+    pushProps() {
+        const propsBody = document.createElement('div'),
+            propsTitle = document.createElement('h2');
+
+        propsBody.classList.add('props-list__item');
+        propsTitle.classList.add('props-list__item__title');
+
+        propsTitle.innerText = this.title;
+
+        propsBody.append(propsTitle);
+
+        propsList.prepend(propsBody);
+    }
+}
+
+function filter(text) {
+    if (text.trim().length == 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
