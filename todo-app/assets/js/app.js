f21517e2a48e551a6ee5ade9bc53c25f30ab2626
hist-site <63604237+hist-site@users.noreply.github.com>
2021-12-01 02:42:38 +0200

    in com

--- todo-app/assets/js/app.js
+++ todo-app/assets/js/app.js
@@ -2,7 +2,8 @@ const input = document.querySelector('.input-block__input'),
     input_btn = document.querySelector('.input-block__btn');
 
 input.addEventListener('input', () => {
-    if (input.value.length == 1) {
+    if (input.value.length !== 0) {
         input_btn.classList.toggle('d-flex');
     }
+
 })
