7e07277b6c4e9803909aefb0207ce550abf11a4e
hist-site <63604237+hist-site@users.noreply.github.com>
2021-12-01 04:20:02 +0200

    in com

--- todo-app/assets/js/app.js
+++ todo-app/assets/js/app.js
@@ -1,9 +1,18 @@
 const input = document.querySelector('.input-block__input'),
-    input_btn = document.querySelector('.input-block__btn');
+    input_btn = document.querySelector('.input-block__btn'),
+    propsItem = document.querySelectorAll('.props-list__item'),
+    propsMenu = document.querySelector('.props-menu');
 
 input.addEventListener('input', () => {
     if (input.value.length !== 0) {
-        input_btn.classList.toggle('d-flex');
+        input_btn.classList.add('d-flex');
+    } else {
+        input_btn.classList.remove('d-flex');
     }
+})
 
+propsItem.forEach(elem => {
+    elem.addEventListener('click', (e) => {
+        propsMenu.classList.add('active');
+    })
 })
